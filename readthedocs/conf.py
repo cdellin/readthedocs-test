06216cb0c82bc673628b673ddae6b4edf6f5941f
Chris Dellin <cdellin@gmail.com>
2016-02-21 00:48:30 -0800

    try 5

--- readthedocs/conf.py
+++ readthedocs/conf.py
@@ -1,7 +1,11 @@
 import os
+import subprocess
+
 print(os.listdir('.'))
 
 with open('_build/html/mypage.html','w') as fp:
    fp.write('hello world!')
+
+subprocess.check_call(['doxygen'])
 
 #exit()
